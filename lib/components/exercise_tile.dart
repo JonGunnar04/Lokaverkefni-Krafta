0ae959e03ec19b38c014feef1dcac2eb0dd80485
JonGunnar04 <jon.gunnar.karlsson@gmail.com>
2025-05-21 16:13:55 +0000

    workout page komið

---
+++ lib/components/exercise_tile.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ExerciseTile extends StatelessWidget {
+  final String exerciseName;
+  final String weight;
+  final String reps;
+  final String sets;
+  final bool isCompleted;
+  final void Function(bool?)? onCheckBoxChanged;
+
+  const ExerciseTile({
+    super.key,
+    required this.exerciseName,
+    required this.weight,
+    required this.reps,
+    required this.sets,
+    required this.isCompleted,
+    required this.onCheckBoxChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[100],
+      child: ListTile(
+        title: Text(exerciseName),
+        subtitle: Row(
+          children: [
+            Chip(label: Text('Weight: $weight kg')),
+            Chip(label: Text('Reps: $reps')),
+            Chip(label: Text('Sets: $sets')),
+          ],
+        ),
+        trailing: Checkbox(
+          value: isCompleted,
+          onChanged: (value) => onCheckBoxChanged!(value),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/workout_page.dart
+++ lib/pages/workout_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:krafta/components/exercise_tile.dart';
+import 'package:krafta/data/workout_data.dart';
+import 'package:provider/provider.dart';
 
 class WorkoutPage extends StatefulWidget {
   final String workoutName;
@@ -9,10 +12,135 @@ class WorkoutPage extends StatefulWidget {
 }
 
 class _WorkoutPageState extends State<WorkoutPage> {
+  // Stýrir því hvort æfing sé lokið eða ekki
+  void onCheckBoxChanged(String workoutName, String exerciseName) {
+    // Breytir stöðu æfingarinnar
+    Provider.of<WorkoutData>(
+      context,
+      listen: false,
+    ).checkOffExercise(workoutName, exerciseName);
+  }
+
+  // text controllers
+  final exerciseNameController = TextEditingController();
+  final weightController = TextEditingController();
+  final repsController = TextEditingController();
+  final setsController = TextEditingController();
+
+  // Stýrir því að ný æfing sé búin til
+  void createNewExercise() {
+    showDialog(
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            title: Text('Create new exercise'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: exerciseNameController,
+                  decoration: InputDecoration(hintText: 'Exercise Name'),
+                ),
+                TextField(
+                  controller: weightController,
+                  decoration: InputDecoration(hintText: 'Weight'),
+                ),
+                TextField(
+                  controller: repsController,
+                  decoration: InputDecoration(hintText: 'Reps'),
+                ),
+                TextField(
+                  controller: setsController,
+                  decoration: InputDecoration(hintText: 'Sets'),
+                ),
+              ],
+            ),
+            actions: [
+              // Vista hnappur sendir innslátt áfram
+              MaterialButton(onPressed: save, child: Text('Save')),
+              // Hætta við lokar glugga án vistunar
+              MaterialButton(onPressed: cancel, child: Text('Cancel')),
+            ],
+          ),
+    );
+  }
+
+  void save() {
+    String newExerciceName = exerciseNameController.text;
+    Provider.of<WorkoutData>(context, listen: false).addExercise(
+      widget.workoutName,
+      newExerciceName,
+      weightController.text,
+      repsController.text,
+      setsController.text,
+    );
+
+    Navigator.pop(context);
+    clear();
+  }
+
+  void cancel() {
+    Navigator.pop(context);
+    clear();
+  }
+
+  void clear() {
+    exerciseNameController.clear();
+    weightController.clear();
+    repsController.clear();
+    setsController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.workoutName),),
+    return Consumer<WorkoutData>(
+      builder:
+          (context, value, child) => Scaffold(
+            appBar: AppBar(title: Text(widget.workoutName)),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () => createNewExercise(),
+              child: Icon(Icons.add),
+            ),
+            body: ListView.builder(
+              itemCount: value.numberOfExercisesInWorkout(widget.workoutName),
+              itemBuilder:
+                  (context, index) => ExerciseTile(
+                    exerciseName:
+                        value
+                            .getRelevantWorkout(widget.workoutName)
+                            .exercises[index]
+                            .name,
+                    weight:
+                        value
+                            .getRelevantWorkout(widget.workoutName)
+                            .exercises[index]
+                            .weight,
+                    reps:
+                        value
+                            .getRelevantWorkout(widget.workoutName)
+                            .exercises[index]
+                            .reps,
+                    sets:
+                        value
+                            .getRelevantWorkout(widget.workoutName)
+                            .exercises[index]
+                            .sets,
+                    isCompleted:
+                        value
+                            .getRelevantWorkout(widget.workoutName)
+                            .exercises[index]
+                            .isCompleted,
+                    onCheckBoxChanged:
+                        (val) => onCheckBoxChanged(
+                          widget.workoutName,
+                          value
+                              .getRelevantWorkout(widget.workoutName)
+                              .exercises[index]
+                              .name,
+                        ),
+                  ),
+            ),
+          ),
     );
   }
 }
